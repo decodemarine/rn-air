4684f52abc189117368f5f4bc798a61e67c6d28a
Katie Lamber <katielamber02@gmail.com>
2019-12-06 18:22:22 +0200

    first commit

---
+++ packages/web/src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+
+import * as serviceWorker from "./serviceWorker";
+import { client } from "./apollo";
+import { Routes } from "./routes";
+import App from "./App";
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Routes />,
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
+);
+serviceWorker.unregister();
